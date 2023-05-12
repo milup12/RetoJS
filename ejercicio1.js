b3083dc7e49e9c4918e45a5ddb338c8a5339e736
Camila Peña <acpenacturismo@gmail.com>
2023-05-11 23:51:21 -0500

    Ejercicio Js 1

---
+++ ejercicio1.js
@@ -0,0 +1,27 @@
+var precioBase = 100;
+var descuento = 0.2;
+
+
+function calcularPrecio(precioBase,descuento){
+    
+
+    console.log(calcularPrecio(precioBase,descuento));
+
+    if(precioBase<=0 || (descuento<0 || descuento>100)){
+         return ("Los valores ingresados son invalidos");
+    //     console.log("Los valores ingresados son invalidos");
+
+     }
+
+     if(descuento==0){
+        return (precioBase);
+    //     console.log(precioBase)
+     }
+    if (descuento>0 && descuento <=100){
+        var precioFinal=precioBase-(precioBase*descuento)/100;
+         return ("El precio final con su descuento es: " + precioFinal);
+    //     console.log("El precio final con su descuento es: " + precioFinal)
+
+     }
+
+}
